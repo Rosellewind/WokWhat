76c6b5745fcb0a8aa7b31c387aebd71a52c62315
Roselle Milvich <rosellemilvich@Roselles-MacBook.local>
2013-11-18 11:00:12 -0700

    Initial Commit

---
+++ WokWhat/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WokWhat
+//
+//  Created by Roselle Milvich on 11/18/13.
+//  Copyright (c) 2013 Roselle Milvich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ WokWhatTests/WokWhatTests.m
@@ -0,0 +1,34 @@
+//
+//  WokWhatTests.m
+//  WokWhatTests
+//
+//  Created by Roselle Milvich on 11/18/13.
+//  Copyright (c) 2013 Roselle Milvich. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WokWhatTests : XCTestCase
+
+@end
+
+@implementation WokWhatTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
